670359f06082cfac8b41b921442273308fb1a696
Duncan McConnell <ddmcconnell4@gmail.com>
2019-10-02 19:39:03 -0500

    Basic JavaFX Code

---
+++ src/com/eat/Test/JavaFXTest.java
@@ -0,0 +1,21 @@
+package com.eat.Test;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class JavaFXTest extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Scene scene = new Scene(new Pane());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
