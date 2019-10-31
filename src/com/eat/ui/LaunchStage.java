5095ec16278e10283c45a4df58baeacabefeed71
Duncan McConnell <ddmcconnell4@gmail.com>
2019-10-31 15:37:24 -0500

    Small aesthetic updates to UI

--- src/com/eat/ui/LaunchStage.java
+++ src/com/eat/ui/LaunchStage.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class LaunchStage extends Application {
@@ -78,7 +79,6 @@ public class LaunchStage extends Application {
 		
 		
 		primaryStage.setScene(currentScene);
-		
 		primaryStage.show();
 		
 	}
@@ -123,7 +123,7 @@ public class LaunchStage extends Application {
 		
 		Stage stage = (Stage) currentScene.getWindow();
 		
-		mainScene = new Scene(mainPane, 1920, 1080);
+		mainScene = new Scene(mainPane, Screen.getPrimary().getBounds().getWidth(), Screen.getPrimary().getBounds().getHeight());
 		currentScene = mainScene;
 		
 		stage.setScene(currentScene);
